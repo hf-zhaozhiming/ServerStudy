dfb51c5f7ee7fc6158e1401c45fc7610016f9f33
hf-zhaozhiming <729144668@qq.com>
2017-06-22 22:10:47 +0800

    fff

---
+++ src/cn/servlet/HttpServlet.java
@@ -0,0 +1,22 @@
+package cn.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HttpServlet
+ */
+public class HttpServlet extends javax.servlet.http.HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
